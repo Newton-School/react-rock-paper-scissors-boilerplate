195f3f83b47f2187ce0e50c58a60e1e1dff72fca
aavishkarmishra <aavishkarmishra@gmail.com>
2022-05-23 15:53:33 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Backdrop, Button } from '@mui/material';
+
+const Result = ({ val }) => {
+  switch (val) {
+    case 0:
+      return <h1 className="">Rock</h1>;
+    case 1:
+      return <h1 className="">Paper</h1>;
+    default:
+      return <h1 className="">Scissors</h1>;
+  }
+};
 
 function App() {
+  const [loading, setLoading] = React.useState(false);
+  const [move, setMove] = React.useState(0);
+  const load = () =>
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  const handleClick = () => {
+    setLoading(true);
+    const val = Math.floor(Math.random() * 4);
+    setMove(val);
+    load();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {loading ? (
+          <img src={logo} className="App-logo" alt="logo" />
+        ) : (
+          <>
+            <Result val={move} />
+            <Button variant="contained" onClick={handleClick}>
+              Start Game
+            </Button>
+          </>
+        )}
       </header>
     </div>
   );
